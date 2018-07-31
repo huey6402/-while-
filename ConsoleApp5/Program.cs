a2ba24d288186a649053c6d343ff4b8d0a55c3fc
huey6402 <41720159+huey6402@users.noreply.github.com>
2018-07-31 14:07:20 +0800

    Initial commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Console.WriteLine("    *    ");
+               Console.WriteLine("   ***   ");
+               Console.WriteLine("  *****  ");
+               Console.WriteLine(" ******* ");
+               Console.WriteLine("*********"); */
+
+            int floor, star , A;
+            floor = 1;
+            A = floor;
+            star = 1;
+
+            while (floor <= 5)
+            {
+                {
+                    while (floor <= 5)
+                    {
+                        Console.Write(" ");
+                        floor = floor + 1;
+                    }
+                }
+                star = star * 2 - 1;
+                while (star >= 1)
+                {
+                    Console.Write("*");
+                    star = star - 1;
+                }
+
+                Console.WriteLine("");
+                A = A + 1;
+                floor = A;
+                star = A;
+            }
+        }
+        
+    }
+}
